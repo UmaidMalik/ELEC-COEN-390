fdc734cc6637e3e8adadd9aad8843e6728e9d814
Zhuoyan Li <2283740802@qq.com>
2021-03-12 18:31:15 -0500

    Info page without inferring constraints

--- UVMe - No Fragments/UVMe/app/src/main/java/com/elec_coen_390/uvme/InfoActivity.java
+++ UVMe - No Fragments/UVMe/app/src/main/java/com/elec_coen_390/uvme/InfoActivity.java
@@ -1,21 +1,98 @@
 package com.elec_coen_390.uvme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.getSupportActionBar().hide();
         setContentView(R.layout.activity_info);
+        this.getSupportActionBar().hide();
 
 
 
-        TextView title = (TextView) findViewById(R.id.activityInfo);
-        title.setText("Info");
+        ImageView ic_sun = (ImageView) findViewById(R.id.ic_sun);
+
+        /*
+         ic_sun.setImageResource(R.drawable.ic_sunlight_default_level1_lightblue);
+         ic_sun.setImageResource(R.drawable.ic_sunlight_level2);
+         ic_sun.setImageResource(R.drawable.ic_sunlight_level3);
+         ic_sun.setImageResource(R.drawable.ic_sunlight_level4);
+         ic_sun.setImageResource(R.drawable.ic_sunlight_level5);
+         */
+
+        setupBottomNavigationListener();
+
+
+
+
+
     }
+
+    private void setupBottomNavigationListener() {
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(1); // bottom navigation menu index item {0(Profile),1(Home),2(More)}
+        menuItem.setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.action_profile:
+                        goToProfileActivity();
+                        break;
+
+                    case R.id.action_more:
+                        goToMoreActivity();
+                        break;
+
+                    default:
+
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    protected void goToProfileActivity() {
+        Intent intentProfile = new Intent(this, ProfileActivity.class);
+        startActivity(intentProfile);
+        finish();
+    }
+
+    protected void goToMoreActivity() {
+        Intent intentMore = new Intent(this, MoreActivity.class);
+        startActivity(intentMore);
+        finish();
+    }
+
+
 }
+
+
+
+
+

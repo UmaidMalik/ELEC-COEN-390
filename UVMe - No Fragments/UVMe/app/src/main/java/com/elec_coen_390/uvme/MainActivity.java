3f727d356d11cd538dc361714117fb509e11a1a7
Vivek <saagarcruz@gmail.com>
2021-03-25 11:35:39 -0400

    dependency added: for location Use location button - UVI & Temp display field - "incomplete"

--- UVMe - No Fragments/UVMe/app/src/main/java/com/elec_coen_390/uvme/MainActivity.java
+++ UVMe - No Fragments/UVMe/app/src/main/java/com/elec_coen_390/uvme/MainActivity.java
@@ -3,12 +3,20 @@ package com.elec_coen_390.uvme;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
@@ -21,6 +29,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,16 +43,83 @@ public class MainActivity extends AppCompatActivity {
     AlertDialog.Builder builder;
     public static final String TAG = "Main Activity";
 
-     Button button;
-     int count = 0;
+    Button button;
+    int count = 0;
+
+    /*
+    // Current location: Initializing the variable
+
+    // in string.xml
+
+    <string name="map key" translatable="false">AIzaSyCDhBHjPpQ64TF7NRswV6NnSkTJxU0JVGo</string>
+
+    API key generated: AIzaSyCDhBHjPpQ64TF7NRswV6NnSkTJxU0JVGo
+
+    Button btLocation;
+    TextView tvCity, tvUVI, tvTemp;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         this.getSupportActionBar().hide();
 
-
         setContentView(R.layout.activity_main);
+
+        /*
+        // Current Location: Assigning variable
+        btLocation = findViewById(R.id.bt_Location);
+        tvCity = findViewById(R.id.tv_City);
+        tvUVI = findViewById(R.id.tv_UVI);
+        tvTemp = findViewById(R.id.tv_Temp);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        btLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Check condition
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                    // When permission granted: call method
+
+                    getCurrentLocation();
+
+                } else {
+
+                    // when permission is not granted: Request permission
+
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+
+
+                }
+            }
+        });
+
+        @Override
+        public void onRequestPermissionResult ( int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults){
+        if (requestCode == 100 && grantResults.length > 0 && (grantResults[0] + grantResults[1] ==
+
+        PackageManager.PERMISSION_GRANTED)){
+
+    // when permission granted: call method
+
+        getCurrentLocation();
+    } else{
+
+            // when permission is denied: display error
+
+            Toast.makeText(getApplicationContext(), "Permission Denied!", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    */
 
         builder = new AlertDialog.Builder(this, R.style.AlertDialogStyle);
 
@@ -49,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         boolean firstStart = prefs.getBoolean("firstStart", true);
 
         if (firstStart) {
-            Log.d(TAG,"Enter a Statement");
+            Log.d(TAG, "Enter a Statement");
             builder.setTitle(R.string.terms_of_services);
             builder.setMessage(R.string.warning_label)
                     .setCancelable(true)
@@ -93,42 +175,143 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 count++;
-                        count = count % 5;
+                count = count % 5;
 
-                        switch (count) {
+                switch (count) {
 
-                            case 0:
-                                ic_sun.setImageResource(R.drawable.ic_sunlight_default_level1_lightblue);
-                                break;
-                            case 1:
-                                ic_sun.setImageResource(R.drawable.ic_sunlight_level2);
-                                break;
-                            case 2:
-                                ic_sun.setImageResource(R.drawable.ic_sunlight_level3);
-                                break;
-                            case 3:
-                                ic_sun.setImageResource(R.drawable.ic_sunlight_level4);
-                                break;
-                            case 4:
-                                ic_sun.setImageResource(R.drawable.ic_sunlight_level5);
-                                break;
-                        }
-
-                System.out.println("Count = " + count);
+                    case 0:
+                        ic_sun.setImageResource(R.drawable.ic_sunlight_default_level1_lightblue);
+                        break;
+                    case 1:
+                        ic_sun.setImageResource(R.drawable.ic_sunlight_level2);
+                        break;
+                    case 2:
+                        ic_sun.setImageResource(R.drawable.ic_sunlight_level3);
+                        break;
+                    case 3:
+                        ic_sun.setImageResource(R.drawable.ic_sunlight_level4);
+                        break;
+                    case 4:
+                        ic_sun.setImageResource(R.drawable.ic_sunlight_level5);
+                        break;
                 }
 
+                System.out.println("Count = " + count);
+            }
+
         });
-
-
 
 
         setupBottomNavigationListener();
 
 
+    }
+
+    
+    /*
+    private void getCurrentLocation() {
+        // Initialize location manager
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Check Condition
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(locationManager.NETWORK_PROVIDER)) {
+
+            // When location service is enabled: Get last location
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+
+                    // Initialize Location
+
+                    Location location = task.getResult();
+
+                    // Check Condition
+
+                    if (location != null) {
+
+                        // when location result is not null: set City - cannot find get City
+
+                        tvCity.setText(string.valueof(location.getLatitude()));
+                    } else {
+
+                        // when location is null: initialize location request
+
+                        LocationRequest locationRequest = new LocationRequest()
+                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                .setInterval(10000)
+                                .setFastestInterval(1000)
+                                .setNumUpdates(1);
+
+                        // initialize location call back
+
+                        LocationCallback locationCallback = new LocationCallback() {
+
+                            @Override
+
+                            public void onLocationResult(LocationResult locationResult) {
+
+                                // initialize location
+
+                                Location location1 = locationResult.getLastLocation();
+                            }
+                        };
+
+                        // Request Location updates
+
+                        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+
+                    }
+
+                }
+
+            });
+
+        }else{
+
+            // when location service is not enabled: Open location setting
+
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
 
 
+            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
 
     }
+
+    */
 
     private void setupBottomNavigationListener() {
 
@@ -176,6 +359,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intentMore);
         finish();
     }
-
 
 }
